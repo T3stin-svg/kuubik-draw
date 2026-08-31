5bfa1cb934a6e14a6c4034f25cdf421162cbca92
Reio <reiosirendi@gmail.com>
2026-08-31 16:18:10 +0300

    chore(integration): export precision modules

--- packages/cad-renderer/src/index.ts
+++ packages/cad-renderer/src/index.ts
@@ -2,3 +2,6 @@ export * from "./bounds.js";
 export * from "./renderer.js";
 export * from "./rtree.js";
 export * from "./selection.js";
+export * from "./snap.js";
+export * from "./tracking.js";
+export * from "./selection-index.js";
