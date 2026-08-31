6613f41ee296ca61fd45be105396868f75e8f8c5
Reio <reiosirendi@gmail.com>
2026-08-31 18:59:51 +0300

    chore(core): export CIRCLE command

--- packages/cad-core/src/index.ts
+++ packages/cad-core/src/index.ts
@@ -3,6 +3,7 @@ export * from "./commands.js";
 export * from "./document.js";
 export * from "./fillet.js";
 export * from "./chamfer.js";
+export * from "./circle-command.js";
 export * from "./break.js";
 export * from "./stretch.js";
 export * from "./match-properties.js";
